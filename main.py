ab16ef619a5f11f6a5e93aa1c930c3839674cb44
ACE <91398173+imacekun@users.noreply.github.com>
2021-12-06 19:44:49 +0530

    Update and rename bot (2).py to main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 import json
 import subprocess
 from pyrogram.types.messages_and_media import message
+import helper
 from pyromod import listen
 from pyrogram.types import Message
 import pyrogram
