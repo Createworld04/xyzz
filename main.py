7ea39067c94bebc7e4708289e27d360cc81db929
Createworld04 <94844308+Createworld04@users.noreply.github.com>
2022-05-10 00:29:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -27,9 +27,9 @@ bot = Client(
 )
 logger = logging.getLogger()
 thumb = os.environ.get("THUMB")
-#if thumb.startswith("http://") or thumb.startswith("https://"):
-    #getstatusoutput(f"wget '{thumb}' -O 'thumb.jpg'")
-   # thumb = "thumb.jpg"
+if thumb.startswith("http://") or thumb.startswith("https://"):
+    getstatusoutput(f"wget '{thumb}' -O 'thumb.jpg'")
+   thumb = "thumb.jpg"
 
 
 @bot.on_message(filters.command(["start"]))
@@ -125,16 +125,16 @@ async def account_login(bot: Client, m: Message):
     input4 = message = await bot.listen(editable.chat.id)
     raw_text4 = input4.text
 
-    editable4= await m.reply_text("Now send the **Thumb url** or send **no**")
-    input6 = message = await bot.listen(editable.chat.id)
-    raw_text6 = input6.text
+    #editable4= await m.reply_text("Now send the **Thumb url** or send **no**")
+    #input6 = message = await bot.listen(editable.chat.id)
+    #raw_text6 = input6.text
 
-    thumb = input6.text
-    if thumb.startswith("http://") or thumb.startswith("https://"):
-        getstatusoutput(f"wget '{thumb}' -O 'thumb.jpg'")
-        thumb = "thumb.jpg"
-    else:
-        thumb == "no"
+    #thumb = input6.text
+    #if thumb.startswith("http://") or thumb.startswith("https://"):
+       # getstatusoutput(f"wget '{thumb}' -O 'thumb.jpg'")
+     #   thumb = "thumb.jpg"
+   # else:
+        #thumb == "no"
 
     
     
